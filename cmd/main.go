d76e29c0de6ecf9423ef2845f77abf8da606d9a7
Mathias Maes <watcherwhale@maes.family>
2023-12-30 19:33:59 +0100

    feat: add a way to configure registries (#2)

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"github.com/rs/zerolog/log"
+	"github.com/watcherwhale/ords/internal/config"
 	"github.com/watcherwhale/ords/internal/http"
 )
 
 var	version string = "dev"
 
 func main() {
-	err := http.CreateServer(version, log.Logger).Run("0.0.0.0:8080")
+	err := config.ReadConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = http.CreateServer(version, log.Logger).Run("0.0.0.0:8080")
 
 	if err != nil {
 		panic(err)
